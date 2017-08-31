2677e579347c76d279af070c2234b01c5796f4d8
DengShubo <dengshb@bis.com.cn>
2017-08-31 14:37:14 +0800

    Code.5.4 可触摸组件，TouchableHighlight没起作用？

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,4 @@
 // require("./chapter4/chapter4.js");
 // require("./chapter5/chapter5_1.js");
-require("./chapter5/chapter5_3.js");
+// require("./chapter5/chapter5_3.js");
+require("./chapter5/chapter5_4.js");
