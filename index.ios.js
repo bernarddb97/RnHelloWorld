8bdc143e5c28cc817292163dc18839111629e347
DengShubo <dengshb@bis.com.cn>
2017-09-01 15:54:00 +0800

    Chapter7: Test AsyncStorage API and Promise.

--- index.ios.js
+++ index.ios.js
@@ -3,4 +3,5 @@
 // require("./chapter5/chapter5_3.js");
 // require("./chapter5/chapter5_4.js");
 // require("./chapter5/chapter5_5.js");
-require("./chapter6/chapter6_6.js");
+// require("./chapter6/chapter6_6.js");
+require("./chapter7/chapter7_1.js");
