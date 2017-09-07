6c6514caded2937dc75b88d8a564356f1645f06d
DengShubo <dengshb@bis.com.cn>
2017-09-07 17:58:47 +0800

    Chaper3 把程序整理到相应的子目录下.

---
+++ chapter3/chapter3.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import {AppRegistry, BackHandler, Platform} from 'react-native';
+import {Navigator} from 'react-native-deprecated-custom-components'
+import LoginLeaf from './LoginLeaf';
+import WaitingLeaf from './WaitingLeaf';
+
+export default class NaviModule extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.configureScene = this.configureScene.bind(this);
+        this.renderScene = this.renderScene.bind(this);
+        this.handleBackAndroid = this.handleBackAndroid.bind(this);
+    }
+
+    configureScene(route) {
+        return Navigator.SceneConfigs.FadeAndroid;
+    }
+
+    renderScene(router, navigator) {
+        this.navigator = navigator;
+        switch (router.name) {
+            case "login" :
+                return <LoginLeaf navigator={navigator}/>;
+            case "waiting":
+                return <WaitingLeaf navigator={navigator} phoneNumber={router.phoneNumber} userPW={router.userPW}/>;
+        }
+    }
+
+    handleBackAndroid() {
+        if (this.navigator.getCurrentRoutes().length > 1) {
+            this.navigator.pop();
+            return true;
+        }
+        return false;
+    }
+
+    componentDidMount() {
+        if (Platform.OS === "android") {
+            BackHandler.addEventListener("hardwareBackPress", this.handleBackAndroid);
+        }
+    }
+
+    componentWillUnmount() {
+        if (Platform.OS === "android") {
+            BackHandler.removeEventListener("hardwareBackPress", this.handleBackAndroid);
+        }
+    }
+
+    render() {
+        return (
+            <Navigator
+                initialRoute={{name: 'login'}}
+                configureScene={this.configureScene}
+                renderScene={this.renderScene}/>
+        );
+    }
+}
+
+AppRegistry.registerComponent('HelloWorld', () => NaviModule);
