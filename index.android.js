b935f2c0b227cadc690c2d2d8b1f7c51c20336d6
DengShubo <dengshb@bis.com.cn>
2017-09-08 13:57:50 +0800

    Code.13.3.1 WebView。未解决问题：Cannot call determinedVisibility() - never saw a connection for the pid: 10230

--- index.android.js
+++ index.android.js
@@ -8,12 +8,12 @@
 // require("./chapter6/chapter6_6.js");
 // require("./chapter7/chapter7_1.js");
 // require("./chapter8/chapter8_1.js");
-require("./chapter9/chapter9_4.js");
+// require("./chapter9/chapter9_4.js");
 // require("./chapter10/chapter10_4.js");
 // require("./chapter10/chapter10_7.js");
 // require("./chapter11/chapter11_1.js");
 // require("./chapter12/chapter12_3.js");
-
+require("./chapter13/chapter13_1.js");
 
 
 // require("./diary/DiaryMain");
